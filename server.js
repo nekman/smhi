66dc27a4f329aa01db0f9c0cbbdccf64e3cc3466
Nils Ekman <nisse.ekman@gmail.com>
2013-11-27 17:15:39 +0100

    Added index route

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+app.configure(function() {
+  //Static resources
+  app.use('/src', express.static(__dirname + '/src'));
+  app.use('/js', express.static(__dirname + '/src/js'));
+
+  // Location route
+  app.all('/loc/:lat/:lng', function(req, res) {
+    res.redirect('#/loc/'+ req.params.lat +'/' + req.params.lng);
+  });
+
+  // Index routes
+  app.all('/', function(req, res) {
+    res.sendfile('src/index.html', { root: __dirname });
+  });
+
+  // Other, just redirect to index
+  app.all('/*', function(req, res) {
+    res.redirect('/');
+  });
+});
+
+var port = process.env.PORT || 5001;
+app.listen(port, function() {
+ console.log('Listening on', port);
+});
