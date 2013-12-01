5679510844e32b7ba9ade48581085b7989bcdebf
Nils Ekman <nisse.ekman@gmail.com>
2013-12-01 13:48:02 +0100

    Added 'http://' prefix for HTML5 application cache

--- src/js/app.config.js
+++ src/js/app.config.js
@@ -1,8 +1,8 @@
 require({
   paths: {
-    jquery: '//code.jquery.com/jquery-2.0.3.min',
-    backbone: '//cdn.jsdelivr.net/backbonejs/1.1.0/backbone-min',
-    lodash: '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.3.0/lodash.min'
+    jquery: 'http://code.jquery.com/jquery-2.0.3.min',
+    backbone: 'http://cdn.jsdelivr.net/backbonejs/1.1.0/backbone-min',
+    lodash: 'http://cdnjs.cloudflare.com/ajax/libs/lodash.js/2.3.0/lodash.min'
   },
   
   shim: {
