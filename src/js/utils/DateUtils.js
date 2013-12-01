05202ccf68367b505640c832ea6939d71567400f
Nils Ekman <nisse.ekman@gmail.com>
2013-12-01 13:52:05 +0100

    Changed date display

--- src/js/utils/DateUtils.js
+++ src/js/utils/DateUtils.js
@@ -3,17 +3,30 @@ define(function() {
   //http://stackoverflow.com/a/3067896/141363
   var zeroPadding = function(str) {
     return str[1] ? str : '0' + str[0];
+  },
+
+  dayNames = {
+    0: 'Söndag',
+    1: 'Måndag',
+    2: 'Tisdag',
+    3: 'Onsdag',
+    4: 'Torsdag',
+    5: 'Fredag',
+    6: 'Lördag'
   };
 
   return {
+    getName: function(date) {
+      return dayNames[date.getDay()];
+    },
+
     toDateString: function(date) {
       date = date || new Date();
 
-      var yyyy = date.getFullYear().toString(),
-          mm = (date.getMonth() + 1).toString(), // getMonth() is zero-based
+      var mm = (date.getMonth() + 1).toString(), // getMonth() is zero-based
           dd = date.getDate().toString();
 
-      return zeroPadding(dd) +'/'+ zeroPadding(mm) + '-' + yyyy;
+      return this.getName(date) + ' ' + dd + '/' + zeroPadding(mm);
     },
 
     toTimeString: function(date) {
