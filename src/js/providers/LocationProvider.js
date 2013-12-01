6fa4604c3d2e87e483044261b564d6bd9f15e0cd
Nils Ekman <nisse.ekman@gmail.com>
2013-12-01 13:50:54 +0100

    Clean up

--- src/js/providers/LocationProvider.js
+++ src/js/providers/LocationProvider.js
@@ -2,29 +2,33 @@ define([
   'jquery',
   './GPSProvider',
   './GeoIpProvider'
-], function($, gpsProvider, geoIpProvider) {
+], function($, GPSProvider, GeoIpProvider) {
   'use strict';
 
   var fetchByGeoProvider = function(dfd) {
     return function() {
-      geoIpProvider
+      GeoIpProvider
         .fetch()
         .then(dfd.resolve)
         .fail(dfd.reject);
     };
+  },
+
+  LocationProvider = function() {
+    this.gpsProvider = new GPSProvider;
   };
 
-  return {
-    fetch: function() {
-      var dfd = $.Deferred();
+  LocationProvider.prototype.fetch = function() {
+    var dfd = $.Deferred();
 
-      gpsProvider
+    this.gpsProvider
         .fetch()
-        .then(dfd.resolve)
+        .done(dfd.resolve)
         //Failed GPS lookup, get coordinates by GeoIp lookup.
         .fail(fetchByGeoProvider(dfd));
 
-      return dfd.promise();
-    }
+    return dfd.promise();
   };
+
+  return LocationProvider;
 });
